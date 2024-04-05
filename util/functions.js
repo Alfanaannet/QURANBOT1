3568a24832002fcea5eec2a4cb68a60b3d59b1e6
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-04-05 04:09:47 +0200

    Update functions.js

--- util/functions.js
+++ util/functions.js
@@ -9,40 +9,7 @@ const shuruhatik = `█▀ █░█ █░█ █▀█ █░█ █
 const settings = new Database("./config.yml");
 
 async function runAction(auto_run) {
-  // if ((await settings.has("reset")) && (await settings.has("token"))) {
-  //   if (auto_run)
-  //     return await startBot(
-  //       (await settings.get("debug")) || false,
-  //       config,
-  //       settings,
-  //     );
-    // const { action } = await inquirer.prompt({
-    //   name: "action",
-    //   type: "list",
-    //   message: `What is the action you want to do?`,
-    //   choices: [
-    //     { name: "Run the bot", value: 0 },
-    //     { name: "Run the bot with debug mode", value: 1 },
-    //     { name: "Re-setup to put a new token and information", value: 2 },
-    //   ],
-    // });
-
-    // if (action == 0) {
-    //   await settings.set("debug", action);
-    //   return await startBot(false, config, settings);
-    // } else if (action == 1) {
-    //   await settings.set("debug", action);
-    //   return await startBot(true, config, settings);
-    // }
-  // } else {
-    // console.log(`\nDeveloped By \u001b[32;1mShuruhatik#2443\u001b[0m `)
-    // await config.delete(`token`);
-    // await config.set(`token`, "jhhkkkkkkkkkk");
-    // await settings.set("status_bot", "إذاعه القرآن الكريم علي مدار 24 ساعه");
-    // await settings.set("status_type", 3);
-    // await settings.set("reset", "احذف هذا السطر إذا كنت تريد تحط توكن جديد");
     return await runAction(false, settings);
-  // }
 }
 
 function sendDM(member, content, file) {
@@ -79,7 +46,7 @@ async function startProject() {
     // process.stdout.write(`\r\u001b[38;5;${getRandomNumber(230)}m${shuruhatik}\u001b[0m\n\n\u001b[1mﻲﺒﻨﻟﺍ ﻰﻠﻋ ةﻼﺻﻭ رﺎﻔﻐﺘﺳﻻﺍ ﺮﺜﻛﻭ ،ﻪﻠﻟﺍ ﺮﻛﺫ َﺲﻨﺗ ﻻ\u001b[0m`);
     if (int.elapsedTime >= timeEnd) {
       int.pause();
-      await runAction(true);
+      await runAction();
     }
   }, 100).start();
 }
